3f6d9032f1194d0d43cb08f8a62194d1ea14d817
Leonardo Figueira <leofigueiracorreia@yahoo.com.br>
2020-11-09 17:44:01 -0300

    add router dom and routes

--- bombom_de_mel/src/App.tsx
+++ bombom_de_mel/src/App.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
-}
+import Routes from './routes';
+import GlobalStyle from './styles/global';
+
+const App: React.FC = () => (
+  <>
+    <BrowserRouter>
+      <Routes />
+    </BrowserRouter>
+    <GlobalStyle />
+  </>
+);
+
 
 export default App;
